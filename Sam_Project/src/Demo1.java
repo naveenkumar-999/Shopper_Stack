25e69bd69443774e820bfd1334fd3364a9ec03cd
naveenkumar959 <anaveenkumar959@gmail.com>
2024-05-07 09:50:55 +0530

    First commit

---
+++ Sam_Project/src/Demo1.java
@@ -0,0 +1,14 @@
+
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		byte[]arr= {10,20,30,40,50};
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
---
+++ Sam_Project/src/Demo5.java
@@ -0,0 +1,15 @@
+
+
+public class Demo5 {
+
+	public static void main(String[] args) {
+		double[]arr= {10.5,20.9,30.9,40.8,50.5};
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+		
+
+	}
+
+}
